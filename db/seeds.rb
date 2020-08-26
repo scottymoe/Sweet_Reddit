b8e24b4ed6bf0a315b17cf624b87ba01fe966a67
Scott Mohorn <scottmohorn@gmail.com>
2020-08-26 10:04:23 -0600

    pushing seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+s = Sub.create(name: 'Sub1')
+
+s.topics.create(name: 'Topic 1', body: 'here is the body')
+s.topics.create(name: 'Topic 2', body: 'here is the body')
+
+puts 'seeded'
+puts s.topics
